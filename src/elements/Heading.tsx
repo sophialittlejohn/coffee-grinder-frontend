927a9fa7de9019dbefcb9d96294d232dd40df71f
Sophia <littlejohn.sophia@gmail.com>
2021-02-21 00:02:25 +0100

    Add styles props to headings

--- src/elements/Heading.tsx
+++ src/elements/Heading.tsx
@@ -1,19 +1,20 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { CSSObject } from "styled-components";
 import { COLORS, Colors } from "../materials/colors";
 
 const commomStyles = (props: HeadingProps) => {
   return `
     font-weight: ${props.fontWeight || "bold"};
     color: ${COLORS[props.color || "black"]};
-    font-size: ${props.size || "auto"}
-  `;
+    font-size: ${props.size || "auto"};
+`;
 };
 
 type HeadingProps = {
   fontWeight?: "bold" | "regular" | "thin";
   color?: Colors;
   size?: string;
+  styles?: CSSObject;
 };
 
 export const H1: React.FC<HeadingProps> = ({
@@ -31,7 +32,7 @@ export const H1: React.FC<HeadingProps> = ({
 const StyledH1 = styled.h1<HeadingProps>`
   margin-top: 0;
   margin-bottom: 0;
-  ${(props) => commomStyles(props)}
+  ${(props) => commomStyles(props)};
 `;
 
 export const H2: React.FC<HeadingProps> = ({
@@ -49,7 +50,8 @@ export const H2: React.FC<HeadingProps> = ({
 const StyledH2 = styled.h2<HeadingProps>`
   margin-top: 0;
   margin-bottom: 0;
-  ${(props) => commomStyles(props)}
+  ${(props) => commomStyles(props)};
+  ${(props) => props.styles};
 `;
 
 export const H3: React.FC<HeadingProps> = ({
@@ -67,5 +69,6 @@ export const H3: React.FC<HeadingProps> = ({
 const StyledH3 = styled.h3<HeadingProps>`
   margin-top: 0;
   margin-bottom: 0;
-  ${(props) => commomStyles(props)}
+  ${(props) => commomStyles(props)};
+  ${(props) => props.styles};
 `;
