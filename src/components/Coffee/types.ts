e747dd7126d34a4601c9755e1ce1bba1a6d49293
Sophia <littlejohn.sophia@gmail.com>
2021-02-21 23:57:44 +0100

    Add cloudinary image type for coffee card

--- src/components/Coffee/types.ts
+++ src/components/Coffee/types.ts
@@ -17,8 +17,31 @@ export interface Coffee extends Address {
   id: number;
   name: string;
   rating: number;
-  photo: string;
+  photo: string | CloudinaryImage;
   coffeeMachine: CoffeeMachine;
   prices: Price[];
   configurations: Configuration[];
 }
+
+type CloudinaryImage = {
+  asset_id: string;
+  public_id: string;
+  version: number;
+  version_id: string;
+  signature: string;
+  width: number;
+  height: number;
+  format: string;
+  resource_type: string;
+  created_at: string;
+  tags: [];
+  bytes: number;
+  type: string;
+  etag: string;
+  placeholder: false;
+  url: string;
+  secure_url: string;
+  access_mode: string;
+  original_filename: string;
+  original_extension: string;
+};
